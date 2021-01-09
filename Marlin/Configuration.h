fc0b18884f072cae669582dc96c376e9a72c16fe
Dave <d.kneisz@gmail.com>
2021-01-09 17:08:35 +0100

    Add 3D Touch settings

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -737,7 +737,8 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 96 }
+//#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 96 } // Default Extruder
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 415 } // BMG-Clone
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -987,7 +988,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 53, -9, -2.835 }
+#define NOZZLE_TO_PROBE_OFFSET { -47, -6, -2.835 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
